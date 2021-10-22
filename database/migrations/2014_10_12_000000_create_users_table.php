d82cd7649747dd85d67476de1836e9ddfac7a158
andres lopez <andresfelipelg@gmail.com>
2021-10-22 15:55:39 -0500

    organizando la svistas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->integer('cedula');
+            $table->string('direccion');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->unsignedBigInteger('id_role');
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
